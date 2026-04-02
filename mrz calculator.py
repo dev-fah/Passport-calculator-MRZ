7b385fd6e413a1ee73390dba5912198f260ab6d5
dev-fah <fanomezantsoa.rakotojaonasy@konecta.com>
2026-04-02 09:41:54 +0200

    Delete mrz calculator.py

--- mrz calculator.py
+++
@@ -1,7 +0,0 @@
-Traceback (most recent call last):
-  File "/home/repl287/main.py", line 1, in <module>
-    import ipywidgets as widgets
-ModuleNotFoundError: No module named 'ipywidgets'
-
-
-** Process exited - Return Code: 1 **
