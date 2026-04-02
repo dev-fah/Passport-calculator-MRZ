afcdd482c6f58bcc00d4c66074e74bde747aa61d
dev-fah <fanomezantsoa.rakotojaonasy@konecta.com>
2026-04-02 09:29:25 +0200

    Add files via upload

---
+++ mrz calculator.py
@@ -0,0 +1,7 @@
+Traceback (most recent call last):
+  File "/home/repl287/main.py", line 1, in <module>
+    import ipywidgets as widgets
+ModuleNotFoundError: No module named 'ipywidgets'
+
+
+** Process exited - Return Code: 1 **
